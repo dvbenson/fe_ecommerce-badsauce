1fc249e888ccb4c8a25f31c5b8a980c6cbf080e7
dvbenson <112098121+dvbenson@users.noreply.github.com>
2023-05-16 20:39:11 +0100

    retired products route

--- app/api/stripe/products/[id]/route.tsx
+++ app/api/stripe/products/[id]/route.tsx
@@ -1,15 +1,15 @@
-import Stripe from 'stripe';
-import { NextResponse } from 'next/server';
+// import Stripe from 'stripe';
+// import { NextResponse } from 'next/server';
 
-//need to debug this, getting null on insomnia
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const id = searchParams.get('id') as string;
-  const stripe = new Stripe(process.env.STRIPE_SECRET ?? '', {
-    apiVersion: '2022-11-15',
-  });
+// //need to debug this, getting null on insomnia
+// export async function GET(request: Request) {
+//   const { searchParams } = new URL(request.url);
+//   const id = searchParams.get('id') as string;
+//   const stripe = new Stripe(process.env.STRIPE_SECRET ?? '', {
+//     apiVersion: '2022-11-15',
+//   });
 
-  const product = await stripe.products.retrieve(id);
+//   const product = await stripe.products.retrieve(id);
 
-  return NextResponse.json({ product });
-}
+//   return NextResponse.json({ product });
+// }
